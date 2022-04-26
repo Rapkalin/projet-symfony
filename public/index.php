50022b8fadb35dad8e4363df297e660ed72a2b0b
Raphael Kalinowski <raphael@myminicom.com>
2022-04-26 18:23:56 +0200

    install symfony flex

--- public/index.php
+++ public/index.php
@@ -1,9 +1,28 @@
 <?php
+//
+//use App\Kernel;
+//
+//require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+//
+//return function (array $context) {
+//    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+//};
+
+
+// NEW CODE PROVIDED BY SYMFONY FLEX AT https://github.com/symfony/recipes/blob/master/symfony/console/5.3/bin/console
+#!/usr/bin/env php
 
 use App\Kernel;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+
+if (!is_file(dirname(__DIR__).'/vendor/autoload_runtime.php')) {
+    throw new LogicException('Symfony Runtime is missing. Try running "composer require symfony/runtime".');
+}
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $kernel = new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+
+    return new Application($kernel);
 };
